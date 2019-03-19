14f10ad4113da94f659cdcb65d31c2a05a9ea1d7
CDeighton <cullumdeighton@gmail.com>
2019-03-20 11:01:32 +1300

    Basic win and lose cases

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Game {
     private List<char> guesses = new List<char>();
@@ -14,16 +15,41 @@ class Game {
 
     private void Run() {
         while (!IsOver()) {
-            guesses.Add(RequestGuess());
+            DisplayMaskedWord();
+            DisplayGuesses();
 
-            lives--;
+            char guess = RequestGuess();
+            guesses.Add(guess);
+
+            if (!IsCorrect(guess)) lives--;
         }
+
+        DisplayEndGame();
+    }
+
+    private void DisplayMaskedWord() {
+        // TODO mask word
+        Console.WriteLine(word);
+    }
+
+    private void DisplayGuesses() {
+        Console.WriteLine(String.Join(", ", guesses));
+    }
+
+    private void DisplayEndGame() {
+        if (IsWon()) Console.WriteLine("You won");
+        else Console.WriteLine("Loser");
     }
 
     private char RequestGuess() {
-        Console.WriteLine("Make a guess: ");
+        string guess;
 
-        return Console.ReadLine()[0];
+        do {
+            Console.WriteLine("Make a guess (one character only): ");
+            guess = Console.ReadLine();
+        } while(guess.Length != 1);
+
+        return guess[0];
     }
 
     private bool IsOver() {
@@ -31,10 +57,14 @@ class Game {
     }
 
     private bool IsWon() {
-        return false;
+        return word.All((c) => guesses.IndexOf(c) != -1);
     }
 
     private bool IsLost() {
         return lives <= 0;
+    }
+
+    private bool IsCorrect(char guess) {
+        return word.Contains(guess);
     }
 }
