eb8b03d7ed1afce7f665dcc8f58f3e5669ea9096
CDeighton <cullumdeighton@gmail.com>
2019-03-20 14:45:38 +1300

    Implement reasonable MVC split

--- Game.cs
+++ Game.cs
@@ -3,79 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 
 class Game {
-    private List<char> guesses = new List<char>();
+    public List<char> guesses = new List<char>();
     private int lives = 5;
-    private string word;
 
-    public Game(string word) {
+    public Game(Word word) {
         this.word = word;
-
-        Run();
     }
 
-    private void Run() {
-        while (!IsOver()) {
-            DisplayMaskedWord();
-            DisplayGuesses();
+    private Word word { get; set; }
 
-            char guess = RequestGuess();
-            guesses.Add(guess);
+    public void MakeGuess(char guess) {
+        guesses.Add(guess);
 
-            if (!IsCorrect(guess)) lives--;
+        if (!word.CorrectGuess(guess)) {
+            lives--;
         }
-
-        if (IsWon()) DisplayWin();
-        else DisplayLoss();
     }
 
-    private void DisplayMaskedWord() {
-        Console.WriteLine(MaskedWord());
+    public bool InProgress() {
+        return !IsWon() && !IsLost();
     }
 
-    private void DisplayGuesses() {
-        Console.WriteLine(String.Join(", ", guesses));
-    }
-
-    private void DisplayWin() {
-        Console.WriteLine("You won");
-    }
-
-    private void DisplayLoss() {
-        Console.WriteLine("The word was {0}. Loser.", word);
-    }
-
-    private string MaskedWord() {
-        return String.Join("", word.Select((c) => IsGuessed(c) ? c : '_'));
-    }
-
-    private char RequestGuess() {
-        string guess;
-
-        do {
-            Console.WriteLine("Make a guess (one character only): ");
-            guess = Console.ReadLine();
-        } while(guess.Length != 1);
-
-        return guess[0];
-    }
-
-    private bool IsOver() {
-        return IsWon() || IsLost();
-    }
-
-    private bool IsWon() {
-        return word.All((c) => IsGuessed(c));
+    public bool IsWon() {
+        return word.IsGuessed(guesses);
     }
 
     private bool IsLost() {
         return lives <= 0;
-    }
-
-    private bool IsCorrect(char guess) {
-        return word.Contains(guess);
-    }
-
-    private bool IsGuessed(char c) {
-        return guesses.IndexOf(c) != -1;
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,9 +8,33 @@ namespace cs_hangman {
             string[] words = File.ReadAllLines(@"dictionary.txt", Encoding.UTF8);
             Random random = new Random();
 
-            string word = words[random.Next(words.Length)];
+            Word word = new Word(words[random.Next(words.Length)]);
+            Game game = new Game(word);
 
-            new Game(word);
+            while (game.InProgress()) {
+                Console.WriteLine(word.Mask(game.guesses));
+                Console.WriteLine(String.Join(", ", game.guesses));
+
+                game.MakeGuess(RequestGuess());
+            }
+
+            if (game.IsWon()) {
+                Console.WriteLine("You won");
+            }
+            else {
+                Console.WriteLine("The word was {0}. Loser.", word.ToString());
+            }
+        }
+
+        public static char RequestGuess() {
+            string guess;
+
+            do {
+                Console.WriteLine("Make a guess (one character only): ");
+                guess = Console.ReadLine();
+            } while(guess.Length != 1);
+
+            return guess[0];
         }
     }
 }
